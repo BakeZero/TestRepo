3539c09b76bc31047d7ff7f2a94f97ce91a269fc
harry <harry.nguyen.2016@gmail.com>
2023-04-10 13:41:54 -0400

    first commit

---
+++ IntroToCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+Console.WriteLine("Enter a Radius:");
+double radius = double.Parse(Console.ReadLine());
+double area = radius * radius * Math.PI;
+Console.WriteLine("Area: " + area);
+
+double circumference = Math.PI * 2 * radius;
+Console.WriteLine("Circumference: " + circumference);
